1697937e602dc6789dfdca933a9317542ea12270
KyungYeul Choi <dnjsvltm201311681@gmail.com>
2024-10-29 18:01:19 +0900

    1.5 -완 -라우터

--- src/app/(beforeLogin)/i/flow/login/page.tsx
+++ src/app/(beforeLogin)/i/flow/login/page.tsx
@@ -1,5 +1,10 @@
 import LoginModal from "@/app/(beforeLogin)/_component/LoginModal";
 
 export default function Page() {
-  return <LoginModal />;
+  return (
+    <>
+      <div>일반i/flow/login입니다.</div>
+      <LoginModal />
+    </>
+  );
 }
--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -1,25 +1,5 @@
-import Image from "next/image";
-import styles from "../page.module.css";
-import Link from "next/link";
-import zlogo from "../../../public/zlogo.png";
+import Main from "@/app/(beforeLogin)/_component/Main";
 
 export default function Home() {
-  return (
-    <>
-      <div className={styles.left}>
-        <Image src={zlogo} alt="logo" />
-      </div>
-      <div className={styles.right}>
-        <h1>지금 일어 나고 있는일</h1>
-        <h2>지금 가입 하세요.</h2>
-        <Link href="/i/flow/signup" className={styles.signup}>
-          계정 만들기
-        </Link>
-        <h3>이미 트위터에 가입하셨나요?</h3>
-        <Link href="/i/flow/login" className={styles.login}>
-          로그인
-        </Link>
-      </div>
-    </>
-  );
+  return <Main />;
 }
