53838aa3b7eb7c44d26233c32fb2882424429f36
KyungYeul Choi <dnjsvltm201311681@gmail.com>
2024-11-07 18:05:15 +0900

    3.3 -d next-auth를 이용하여 로그인하

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -9,7 +9,6 @@ type Props = {
 export default function Layout({ children, modal }: Props) {
   return (
     <div className={styles.container}>
-      비포 로그인 레이아웃
       {children} {/*일반페이지 */}
       {modal} {/*모달페이지 */}
     </div>
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -10,21 +10,29 @@ const Posts = [];
 
 export const handlers = [
   http.post("/api/login", () => {
-    console.log("login");
-    return HttpResponse.json(null, {
+    console.log("로그인");
+    return HttpResponse.json(User[1], {
       headers: {
         "Set-Cookies": "connect.sid=msw-cookie;HttpOnly;Path=/",
       },
     });
   }),
-  http.post("/api/users", () => {
+  http.post("/api/logout", () => {
+    console.log("로그아웃");
+    return new HttpResponse(null, {
+      headers: {
+        "Set-Cookie": "connect.sid=;HttpOnly;Path=/;Max-Age=0",
+      },
+    });
+  }),
+  http.post("/api/users", async ({ request }) => {
     console.log("회원가입");
-    // return HttpResponse.text(JSON.stringify("user_exists"), {
+    // return HttpResponse.text(JSON.stringify('user_exists'), {
     //   status: 403,
-    // });
+    // })
     return HttpResponse.text(JSON.stringify("ok"), {
       headers: {
-        "Set-Cookies": "connect.sid=;HttpOnly;Path=/;Max-Age=0",
+        "Set-Cookie": "connect.sid=msw-cookie;HttpOnly;Path=/",
       },
     });
   }),
