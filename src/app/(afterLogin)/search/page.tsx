f09e0366e0b2c557477848a6f7541f5cc892f8b6
KyungYeul Choi <dnjsvltm201311681@gmail.com>
2024-11-11 17:30:49 +0900

    3.7 -d -팔로잉게시글/검색결과 불러오기

--- src/app/(afterLogin)/search/page.tsx
+++ src/app/(afterLogin)/search/page.tsx
@@ -6,14 +6,12 @@ import BackButton from "@/app/(afterLogin)/_component/BackButton";
 import SearchForm from "@/app/(afterLogin)/_component/SearchForm";
 import Tab from "@/app/(afterLogin)/search/_conponent/Tab";
 import Post from "@/app/(afterLogin)/_component/Post";
+import SearchResult from "./_conponent/SearchResult";
 
 type Props = {
   searchParams: { q: string; f?: string; pf?: string };
 };
-export default function Search() {
-  const searchParams = useSearchParams(); // 클라이언트 컴포넌트에서 searchParams를 불러옵니다.
-  const q = searchParams.get("q") || ""; // searchParams에서 q 값을 가져옵니다.
-
+export default function Search({ searchParams }: Props) {
   return (
     <main className={style.main}>
       <div className={style.searchTop}>
@@ -28,18 +26,7 @@ export default function Search() {
         <Tab />
       </div>
       <div className={style.list}>
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        {/*<SearchResult searchParams={searchParams} />*/}
+        <SearchResult searchParams={searchParams} />
       </div>
     </main>
   );
