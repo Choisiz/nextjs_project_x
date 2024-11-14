35454ab025af156a580a39385b4f191f226f0060
KyungYeul Choi <dnjsvltm201311681@gmail.com>
2024-11-14 16:29:16 +0900

    3.10 -d -상세페이지,답글(react-query4)

--- src/app/(afterLogin)/[username]/status/[id]/page.tsx
+++ src/app/(afterLogin)/[username]/status/[id]/page.tsx
@@ -2,25 +2,48 @@ import style from "./singlePost.module.css";
 import BackButton from "@/app/(afterLogin)/_component/BackButton";
 import Post from "@/app/(afterLogin)/_component/Post";
 import CommentForm from "./_component/CommentForm";
+import SinglePost from "@/app/(afterLogin)/[username]/status/[id]/_component/SinglePost";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import { getSinglePost } from "./_lib/getSinglePost";
+import { getSinglePostServer } from "./_lib/getSinglePostServer";
+import { getComments } from "./_lib/getComment";
+import Comments from "./_component/Comments";
 
-export default function SinglePost() {
+type Props = {
+  params: { id: string };
+};
+
+export default async function Page({ params }: Props) {
+  const { id } = params;
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["posts", id],
+    queryFn: getSinglePost,
+  });
+  await queryClient.prefetchQuery({
+    queryKey: ["posts", id, "comments"],
+    queryFn: getComments,
+  });
+  const dehydratedState = dehydrate(queryClient);
+  console.log("dddd", dehydratedState);
+
   return (
     <div className={style.main}>
-      <div className={style.header}>
-        <BackButton />
-        <h3 className={style.headerTitle}>게시하기</h3>
-      </div>
-      <Post />
-      <CommentForm />
-      <div>
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-      </div>
+      <HydrationBoundary state={dehydratedState}>
+        <div className={style.header}>
+          <BackButton />
+          <h3 className={style.headerTitle}>게시하기</h3>
+        </div>
+        <SinglePost id={id} />
+        <CommentForm />
+        <div>
+          <Comments id={id} />
+        </div>
+      </HydrationBoundary>
     </div>
   );
 }
