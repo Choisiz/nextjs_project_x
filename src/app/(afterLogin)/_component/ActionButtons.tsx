656ceb84ba614cf655c2e2136bc1ca0521e2a17b
KyungYeul Choi <dnjsvltm201311681@gmail.com>
2024-12-27 10:25:03 +0900

    4.4 -d -좋아요 수정

--- src/app/(afterLogin)/_component/ActionButtons.tsx
+++ src/app/(afterLogin)/_component/ActionButtons.tsx
@@ -9,24 +9,23 @@ import {
 } from "@tanstack/react-query";
 import { useSession } from "next-auth/react";
 import { Post } from "@/model/Post";
-import { useRouter } from "next/navigation";
-import { useModalStore } from "@/store/modal";
 
 type Props = {
   white?: boolean;
   post: Post;
 };
+
 export default function ActionButtons({ white, post }: Props) {
   const queryClient = useQueryClient();
-  const router = useRouter();
   const { data: session } = useSession();
-  const modalStore = useModalStore();
+  const commented = !!post.Comments?.find(
+    (v) => v.userId === session?.user?.email
+  );
   const reposted = !!post.Reposts?.find(
     (v) => v.userId === session?.user?.email
   );
   const liked = !!post.Hearts?.find((v) => v.userId === session?.user?.email);
   const { postId } = post;
-
   const heart = useMutation({
     mutationFn: () => {
       return fetch(
@@ -267,169 +266,12 @@ export default function ActionButtons({ white, post }: Props) {
     onSettled() {},
   });
 
-  const repost = useMutation({
-    mutationFn: () => {
-      return fetch(
-        `${process.env.NEXT_PUBLIC_BASE_URL}/api/posts/${post.postId}/reposts`,
-        {
-          method: "post",
-          credentials: "include",
-        }
-      );
-    },
-    async onSuccess(response) {
-      const data = await response.json();
-      const queryCache = queryClient.getQueryCache();
-      const queryKeys = queryCache.getAll().map((cache) => cache.queryKey);
-      console.log("queryKeys", queryKeys);
-      queryKeys.forEach((queryKey) => {
-        if (queryKey[0] === "posts") {
-          console.log(queryKey[0]);
-          const value: Post | InfiniteData<Post[]> | undefined =
-            queryClient.getQueryData(queryKey);
-          if (value && "pages" in value) {
-            console.log("array", value);
-            const obj = value.pages.flat().find((v) => v.postId === postId);
-            if (obj) {
-              // 존재는 하는지
-              const pageIndex = value.pages.findIndex((page) =>
-                page.includes(obj)
-              );
-              const index = value.pages[pageIndex].findIndex(
-                (v) => v.postId === postId
-              );
-              console.log("found index", index);
-              const shallow = { ...value };
-              value.pages = { ...value.pages };
-              value.pages[pageIndex] = [...value.pages[pageIndex]];
-              shallow.pages[pageIndex][index] = {
-                ...shallow.pages[pageIndex][index],
-                Reposts: [{ userId: session?.user?.email as string }],
-                _count: {
-                  ...shallow.pages[pageIndex][index]._count,
-                  Reposts: shallow.pages[pageIndex][index]._count.Reposts + 1,
-                },
-              };
-              shallow.pages[0].unshift(data);
-              queryClient.setQueryData(queryKey, shallow);
-            }
-          } else if (value) {
-            // 싱글 포스트인 경우
-            if (value.postId === postId) {
-              const shallow = {
-                ...value,
-                Reposts: [{ userId: session?.user?.email as string }],
-                _count: {
-                  ...value._count,
-                  Reposts: value._count.Reposts + 1,
-                },
-              };
-              queryClient.setQueryData(queryKey, shallow);
-            }
-          }
-        }
-      });
-    },
-  });
+  const onClickComment = () => {};
+  const onClickRepost = () => {};
 
-  const deleteRepost = useMutation({
-    mutationFn: () => {
-      return fetch(
-        `${process.env.NEXT_PUBLIC_BASE_URL}/api/posts/${post.postId}/reposts`,
-        {
-          method: "delete",
-          credentials: "include",
-        }
-      );
-    },
-    onSuccess() {
-      const queryCache = queryClient.getQueryCache();
-      const queryKeys = queryCache.getAll().map((cache) => cache.queryKey);
-      console.log("queryKeys", queryKeys);
-      queryKeys.forEach((queryKey) => {
-        if (queryKey[0] === "posts") {
-          const value: Post | InfiniteData<Post[]> | undefined =
-            queryClient.getQueryData(queryKey);
-          if (value && "pages" in value) {
-            console.log("array", value);
-            const obj = value.pages.flat().find((v) => v.postId === postId);
-            const repost = value.pages
-              .flat()
-              .find(
-                (v) =>
-                  v.Original?.postId === postId &&
-                  v.User.id === session?.user?.email
-              );
-            if (obj) {
-              // 존재는 하는지
-              const pageIndex = value.pages.findIndex((page) =>
-                page.includes(obj)
-              );
-              const index = value.pages[pageIndex].findIndex(
-                (v) => v.postId === postId
-              );
-              console.log("found index", index);
-              const shallow = { ...value };
-              value.pages = { ...value.pages };
-              value.pages[pageIndex] = [...value.pages[pageIndex]];
-              shallow.pages[pageIndex][index] = {
-                ...shallow.pages[pageIndex][index],
-                Reposts: shallow.pages[pageIndex][index].Reposts.filter(
-                  (v) => v.userId !== session?.user?.email
-                ),
-                _count: {
-                  ...shallow.pages[pageIndex][index]._count,
-                  Reposts: shallow.pages[pageIndex][index]._count.Reposts - 1,
-                },
-              };
-              // 재게시 삭제
-              shallow.pages = shallow.pages.map((page) => {
-                return page.filter((v) => v.postId !== repost?.postId);
-              });
-              queryClient.setQueryData(queryKey, shallow);
-            }
-          } else if (value) {
-            // 싱글 포스트인 경우
-            if (value.postId === postId) {
-              const shallow = {
-                ...value,
-                Reposts: value.Reposts.filter(
-                  (v) => v.userId !== session?.user?.email
-                ),
-                _count: {
-                  ...value._count,
-                  Reposts: value._count.Reposts - 1,
-                },
-              };
-              queryClient.setQueryData(queryKey, shallow);
-            }
-          }
-        }
-      });
-    },
-  });
-
-  const onClickComment: MouseEventHandler<HTMLButtonElement> = (e) => {
-    e.stopPropagation();
-
-    modalStore.setMode("comment");
-    modalStore.setData(post);
-    router.push("/compose/tweet");
-  };
-
-  const onClickRepost: MouseEventHandler<HTMLButtonElement> = (e) => {
-    e.stopPropagation();
-    e.preventDefault();
-    if (!reposted) {
-      repost.mutate();
-    } else {
-      deleteRepost.mutate();
-    }
-  };
   const onClickHeart: MouseEventHandler<HTMLButtonElement> = (e) => {
     e.stopPropagation();
     if (liked) {
-      console.log("e", e);
       unheart.mutate();
     } else {
       heart.mutate();
